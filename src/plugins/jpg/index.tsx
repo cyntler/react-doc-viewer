34bed0c7e1c33a7f1bc6eb4b5895945913a49586
Matthew Mogford <matthew.mogford@alcumusgroup.com>
2020-08-28 15:55:03 +0100

    feat(Merge Default Plugins): Plugins have been pulled back into the component package, Updated README to inform on updated and better way to use included and custom plugins

---
+++ src/plugins/jpg/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { DocViewerState } from "../../state";
+import { DocRenderer } from "../../types";
+
+const JPGRenderer: DocRenderer = () => {
+  const currentDocument = useRecoilValue(DocViewerState.currentDocument);
+
+  if (!currentDocument) return null;
+
+  return (
+    <Container id="jpg-renderer">
+      <Img id="jpg-img" src={currentDocument.base64Data} />
+    </Container>
+  );
+};
+
+export default JPGRenderer;
+
+JPGRenderer.fileTypes = ["image/jpg", "image/jpeg"];
+JPGRenderer.weight = 0;
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  background-color: #fff;
+`;
+
+const Img = styled.img`
+  width: 50%;
+`;
