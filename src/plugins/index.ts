2e8578b127f61e855cab9371fb32f193eea88742
Matthew Mogford <matthew.mogford@alcumusgroup.com>
2020-09-24 15:28:17 +0100

    feat: BMP Image renderer added

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
+import BMPRenderer from "./bmp";
 import JPGRenderer from "./jpg";
 import MSDocRenderer from "./msdoc";
 import MSGRenderer from "./msg";
@@ -5,6 +6,7 @@ import PDFRenderer from "./pdf";
 import PNGRenderer from "./png";
 
 export const DocViewerRenderers = [
+  BMPRenderer,
   JPGRenderer,
   MSDocRenderer,
   MSGRenderer,
