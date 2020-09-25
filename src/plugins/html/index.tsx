fd3e79dc832b687a42f8215f6bf402d7ec73be0e
Matthew Mogford <69457935+mattmogford-alcumus@users.noreply.github.com>
2020-09-25 16:35:43 +0100

    Develop (#42)
    * docs: README - styled component example is missing a closing bracket after DocViewer
    Fixes #31
    
    * feat: Internal ImageProxyRenderer created, that can be used by all other Image renderers, and styled by them
    
    * feat: BMP Image renderer added
    
    * Create TIFF renderer
    Fixes #33
    
    * MSGRenderer - Use safe iframe for msg content display (#39)
    
    * docs: README - styled component example is missing a closing bracket after DocViewer
    Fixes #31
    
    * feat: Internal ImageProxyRenderer created, that can be used by all other Image renderers, and styled by them
    
    * feat: BMP Image renderer added
    
    * Create TIFF renderer
    Fixes #33
    
    * updated CHANGELOG.md
    
    * 0.0.40
    
    * MSGRenderer - Use safe iframe for msg content display
    Fixes #38
    
    * Create HTMLRenderer
    Fixes #40

---
+++ src/plugins/html/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { DocRenderer, IStyledProps } from "../../types";
+import { base64FileLoader } from "../../utils/fileLoaders";
+
+const HTMLRenderer: DocRenderer = ({ mainState: { currentDocument } }) => {
+  useEffect(() => {
+    const bodyBase64 =
+      currentDocument?.base64Data?.replace("data:text/html;base64,", "") || "";
+    const body: string = window.atob(bodyBase64);
+
+    let iframeCont = document.getElementById(
+      "html-body"
+    ) as HTMLIFrameElement | null;
+    let iframe = iframeCont?.contentWindow && iframeCont.contentWindow;
+    if (!iframe) return;
+
+    const iframeDoc = iframe.document;
+    iframeDoc.open();
+    iframeDoc.write(`${body}`);
+    iframeDoc.close();
+  }, []);
+
+  return (
+    <Container id="html-renderer">
+      <BodyIFrame id="html-body" sandbox="allow-same-origin" />
+    </Container>
+  );
+};
+
+export default HTMLRenderer;
+
+HTMLRenderer.fileTypes = ["htm", "html", "text/htm", "text/html"];
+HTMLRenderer.weight = 0;
+HTMLRenderer.fileLoader = base64FileLoader;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0 30px;
+`;
+
+const BodyIFrame = styled.iframe`
+  height: 100%;
+  padding: 15px;
+  margin: 20px 0 20px 0;
+  border: 1px solid ${(props: IStyledProps) => props.theme.secondary};
+`;
