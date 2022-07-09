345e4db2267e42190ae8522bb6c7dce718cfe0f0
Damian Cyntler <damian@cyntler.com>
2022-07-09 09:41:22 +0200

    fix: loader fetch headers

--- src/utils/fileLoaders.ts
+++ src/utils/fileLoaders.ts
@@ -2,7 +2,9 @@ export interface FileLoaderFuncProps {
   documentURI: string;
   signal: AbortSignal;
   fileLoaderComplete: FileLoaderComplete;
+  headers?: Record<string, string>;
 }
+
 export type FileLoaderComplete = (fileReader?: FileReader) => void;
 export type FileLoaderFunction = (props: FileLoaderFuncProps) => void;
 
@@ -11,14 +13,17 @@ type ReaderTypeFunction = "dataURL" | "arrayBuffer" | "binaryString" | "text";
 interface BaseFileLoaderFuncOptions extends FileLoaderFuncProps {
   readerTypeFunction: ReaderTypeFunction;
 }
+
 type BaseFileLoaderFunction = (props: BaseFileLoaderFuncOptions) => void;
+
 const _fileLoader: BaseFileLoaderFunction = ({
   documentURI,
   signal,
   fileLoaderComplete,
   readerTypeFunction,
+  headers,
 }) => {
-  return fetch(documentURI, { signal })
+  return fetch(documentURI, { signal, headers })
     .then(async (res) => {
       const blob = await res.blob();
 
