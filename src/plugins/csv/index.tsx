f5c2ae2aa7de954b1d03692fea57f997603812ad
Damian Cyntler <damian@cyntler.com>
2022-08-18 23:45:01 +0200

    Add csv renderer

--- src/plugins/csv/index.tsx
+++ src/plugins/csv/index.tsx
@@ -1,13 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { CsvToHtmlTable } from "react-csv-to-table";
+import styled from "styled-components";
 import { DocRenderer } from "../../types";
 import { textFileLoader } from "../../utils/fileLoaders";
 
-const HTMLRenderer: DocRenderer = ({ mainState: { currentDocument } }) => {
+const HTMLRenderer: DocRenderer = ({
+  mainState: { currentDocument, config },
+}) => {
+  const [data, setData] = useState("");
+
   useEffect(() => {
-    console.log(currentDocument?.fileData);
+    if (currentDocument?.fileData) {
+      setData(currentDocument.fileData as string);
+    }
   }, [currentDocument]);
 
-  return <div>asdasdasdasd</div>;
+  if (!data) return null;
+  return (
+    <Container>
+      <CsvToHtmlTable
+        data={data}
+        csvDelimiter={config?.csvDelimiter ?? ","}
+        tableClassName="table"
+      />
+    </Container>
+  );
 };
 
 export default HTMLRenderer;
@@ -15,3 +32,17 @@ export default HTMLRenderer;
 HTMLRenderer.fileTypes = ["csv", "text/csv"];
 HTMLRenderer.weight = 0;
 HTMLRenderer.fileLoader = textFileLoader;
+
+const Container = styled.div`
+  width: 100%;
+
+  .table {
+    width: 100%;
+    text-align: left;
+
+    th,
+    td {
+      padding: 5px 10px;
+    }
+  }
+`;
