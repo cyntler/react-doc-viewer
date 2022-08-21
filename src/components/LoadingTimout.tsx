1e86a7691ad8e273e33ce16adb6ab10fa35a9788
Damian Cyntler <damian@cyntler.com>
2022-08-21 18:00:01 +0200

    Add LoadingTimeout component

---
+++ src/components/LoadingTimout.tsx
@@ -0,0 +1,35 @@
+import React, {
+  FC,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { DocViewerContext } from "../state";
+
+export const LoadingTimeout: FC<PropsWithChildren> = ({ children }) => {
+  const { state } = useContext(DocViewerContext);
+  const { config } = state;
+  const [shouldLoadingRender, setShouldLoadingRender] = useState(
+    config?.loadingRenderer?.showLoadingTimeout === false
+  );
+
+  console.log("shouldLoadingRender", shouldLoadingRender);
+
+  useEffect(() => {
+    setTimeout(
+      () => {
+        setShouldLoadingRender(true);
+      },
+      typeof config?.loadingRenderer?.showLoadingTimeout === "number"
+        ? config.loadingRenderer.showLoadingTimeout
+        : 500
+    );
+  }, [config?.loadingRenderer?.showLoadingTimeout]);
+
+  if (!shouldLoadingRender) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
