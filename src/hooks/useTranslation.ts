83553a0a6807da6cc3fae4d3e5e85685180e2af4
Damian Cyntler <damian@cyntler.com>
2022-09-03 12:57:12 +0200

    Implement mustache

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,16 +1,22 @@
 import { useCallback, useContext } from "react";
+import mustache from "mustache";
 import { DocViewerContext } from "../store/DocViewerProvider";
 import { locales } from "../utils/i18n";
 
-export const useTranslation = (key: string) => {
+export const useTranslation = () => {
   const {
     state: { language },
   } = useContext(DocViewerContext);
 
-  const t = useCallback(() => {
-    const translations = locales[language];
-    return translations[key] ?? key;
-  }, [key, language]);
+  const t = useCallback(
+    (key: string, variables?: Record<string, string | number>) => {
+      const translations = locales[language];
+      return translations[key]
+        ? mustache.render(translations[key], variables)
+        : key;
+    },
+    [language]
+  );
 
   return {
     t,
