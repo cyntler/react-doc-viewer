475aa35a94931e702abc80deb8720b6ec4987430
Damian Cyntler <damian@cyntler.com>
2022-09-04 12:48:59 +0200

    Add pl locale

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,7 +1,9 @@
 import en from "../locales/en.json";
+import pl from "../locales/pl.json";
 
 export const locales = {
   en: en as Record<string, string>,
+  pl: pl as Record<string, string>,
 };
 
 export type AvailableLanguages = keyof typeof locales;
