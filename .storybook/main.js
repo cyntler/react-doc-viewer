568985fd81b777d445f53e426177c119585bf189
Damian Cyntler <damian@cyntler.com>
2023-10-08 18:34:33 +0200

    chore: migrate to sb v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,17 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(txt|csv|eps|mmdb)$/,
@@ -25,4 +28,8 @@ module.exports = {
 
     return config;
   },
+
+  docs: {
+    autodocs: true
+  }
 };
