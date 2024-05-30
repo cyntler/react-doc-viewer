b968b8f505ae0cd361ee6469d7e8a204216e190e
Damian Cyntler <damian@cyntler.com>
2024-05-30 21:04:26 +0200

    fix: prettier check

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
