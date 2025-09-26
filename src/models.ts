b136d50923b78ab038e32e69c00cfd1c61216d01
toma <toma.karadole@unitfly.com>
2025-09-26 16:07:53 +0200

    adding override for pdfControls

--- src/models.ts
+++ src/models.ts
@@ -1,12 +1,14 @@
 import { FC, ReactElement, ComponentType, PropsWithChildren } from "react";
 import { IMainState } from "./store/mainStateReducer";
 import { FileLoaderFunction } from "./utils/fileLoaders";
+import { IPDFState } from "./renderers/pdf/state/reducer";
 
 export interface IConfig {
   header?: IHeaderConfig;
   loadingRenderer?: ILoadingRendererConfig;
   noRenderer?: INoRendererConfig;
   csvDelimiter?: string;
+  pdfControls?: IPdfControlsConfig;
   pdfZoom?: IPdfZoomConfig;
   pdfVerticalScrollByDefault?: boolean;
 }
@@ -33,6 +35,19 @@ export interface IHeaderConfig {
   overrideComponent?: IHeaderOverride;
 }
 
+export interface IPdfControlsConfig {
+  disableControls?: boolean;
+  disableZoom?: boolean;
+  disablePagination?: boolean;
+  disableDownload?: boolean;
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
+  paginated?: boolean;
+  initialZoom?: number;
+  zoomJump?: number;
+  defaultZoom?: number;
+  overrideComponent?: IPdfControlsOverride;
+}
+
 export interface IPdfZoomConfig {
   defaultZoom: number;
   zoomJump: number;
@@ -44,6 +59,17 @@ export type IHeaderOverride = (
   nextDocument: () => void,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ) => ReactElement<any, any> | null;
+
+export type IPdfControlsOverride = {
+  (
+    state: IPDFState,
+    pdfZoomOut?: () => void,
+    pdfZoomIn?: () => void,
+    pdfZoomReset?: () => void,
+    pdfTogglePaginated?: () => void,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): ReactElement<any, any> | null;
+};
 
 export interface ITheme {
   primary?: string;
